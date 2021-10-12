d01cc6e1ab01aed86a7b987f0006f45e4d39cf3c
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2021-10-12 21:33:42 +0200

    half of iteration 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import contacts from './contacts.json';
+
+const producerContacts = contacts;
+
+class DisplayContacts extends React.Component {
+  constructor() {
+    super();
+    this.state = { contacts: producerContacts.slice(0, 5) };
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.contacts.map((contact) => {
+          return <li key={contact.id}>{contact.name}</li>;
+        })}
+      </ul>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DisplayContacts />
     </div>
   );
 }
