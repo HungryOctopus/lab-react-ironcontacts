30fe829586eb79839ee82d550df7d8bce5afd2a6
Amélie Monnoyer <49373560+HungryOctopus@users.noreply.github.com>
2021-10-13 18:31:32 +0200

    iteration 2 completed

--- src/App.js
+++ src/App.js
@@ -10,13 +10,25 @@ class App extends React.Component {
     this.state = { contacts: producerContacts.slice(0, 5) };
   }
 
+  addRandomContact = (event) => {
+    event.preventDefault();
+    const newContact =
+      producerContacts[Math.floor(Math.random() * producerContacts.length)];
+    console.log(newContact);
+    this.setState((currentState) => {
+      return {
+        contacts: [...currentState.contacts, newContact]
+      };
+    });
+  };
+
   render() {
     return (
       <main>
         <table>
           <thead>
             <tr>
-              <th colspan="3">Ironcontacts</th>
+              <th colSpan="3">Ironcontacts</th>
             </tr>
           </thead>
           <tbody>
@@ -43,6 +55,9 @@ class App extends React.Component {
             })}
           </tbody>
         </table>
+        <form onSubmit={this.addRandomContact}>
+          <button>Add random contact</button>
+        </form>
       </main>
     );
   }
